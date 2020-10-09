7e90f42697deac16cc805a4364cc8f457a7c0bda
Achraf <achrafboularhmane1@gmail.com>
2020-10-09 15:04:44 +0100

    desinfection services

--- script.js
+++ script.js
@@ -40,4 +40,14 @@ $(document).ready(function(){
     });
 });
 
+// images slider // 
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+},4000);
+
 
